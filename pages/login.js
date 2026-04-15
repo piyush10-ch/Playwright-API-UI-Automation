534612fb73fdee4df5e6c89b1c2f25d78e184851
Piyush <Piyushchaudhary564449@gmail.com>
2026-04-15 10:56:58 +0530

    fix pom

--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,27 @@
-import {expect} from '@playwright/test'
-export class LoginPage{
-    constructor(page){
-        this.page=page
-        this.username=page.getByPlaceholder('Username')
-        this.password=page.getByPlaceholder('Password')
-        this.loginbtn=page.getByRole('button',{name:'Login'})
-        this.dashboardcheck=page.getByRole('heading',{name:'Dashboard'})
-        this.clickpim=page.getByRole('link',{name:'PIM'})
-        this.myinfo=page.getByRole('link',{name:'My Info'})}
+    export class LoginPage{
+        constructor(page){
+            this.page=page
+            this.username=page.getByPlaceholder('Username')
+            this.password=page.getByPlaceholder('Password')
+            this.loginbtn=page.getByRole('button',{name:'Login'})
+        
+            this.clickpim=page.getByRole('link',{name:'PIM'})
+            this.myinfo=page.getByRole('link',{name:'My Info'})}
 
-    async goto() {   // ← yeh add karo
-        await this.page.goto('https://opensource-demo.orangehrmlive.com/')
-    }
-       
-    async login(un,pw){
-        await this.username.fill(un)
-        await this.password.fill(pw)
-        await this.loginbtn.click()
-    }
-    async isLoaded(){
-        await expect(this.dashboardcheck).toBeVisible({timeout:15000})
-    }
-    async clickPIM(){
-        await this.clickpim.click()
-    }
-    async clickMyInfo(){
-        await this.myinfo.click()
+        
+        
+        async login(un,pw){
+            await this.username.fill(un)
+            await this.password.fill(pw)
+            await this.loginbtn.click()
+        }
+    
+        async clickPIM(){
+            await this.clickpim.click()
+        }
+        async clickMyInfo(){
+            await this.myinfo.click()
+
+        }
 
     }
-
-}
